023fc9ba1713548b9bc0277a82733152d87241a1
劉晏儒 <ruru@liuyanrudeMacBook-Pro.local>
2024-06-12 08:31:46 +0800

    spcing adjust

--- routers/signup.py
+++ routers/signup.py
@@ -24,20 +24,20 @@ async def signUp(request:Request,user:checkAccount):
   email = user.email
   password = hash_password(user.password)
 
-  db_pool = request.state.db_pool.get("member")
-  try:
-    with db_pool.get_connection() as con:
-        with con.cursor(dictionary = True) as cursour:
-            cursour.execute("select id from account where email = %s",(email,))
-            existing_user = cursour.fetchone()
-            if existing_user : 
-                data = {"error": True, "message": "註冊失敗，重複的 Email"}
-                return JSONResponse(status_code=400, content=data, media_type="application/json")
-            cursour.execute("insert into account(name,email,password) values(%s,%s,%s)",(name,email,password))
-            con.commit()
-            data = {"ok": True}
-            return JSONResponse(content=data, media_type="application/json")
-  except Exception as e:
-        print(f"Unhandled exception: {e}")
-        data = {"error": True, "message": "伺服器內部錯誤"}
-        return JSONResponse(status_code=500, content=data, media_type="application/json")
+#   db_pool = request.state.db_pool.get("member")
+#   try:
+#     with db_pool.get_connection() as con:
+#         with con.cursor(dictionary = True) as cursour:
+#             cursour.execute("select id from account where email = %s",(email,))
+#             existing_user = cursour.fetchone()
+#             if existing_user : 
+#                 data = {"error": True, "message": "註冊失敗，重複的 Email"}
+#                 return JSONResponse(status_code=400, content=data, media_type="application/json")
+#             cursour.execute("insert into account(name,email,password) values(%s,%s,%s)",(name,email,password))
+#             con.commit()
+#             data = {"ok": True}
+#             return JSONResponse(content=data, media_type="application/json")
+#   except Exception as e:
+#         print(f"Unhandled exception: {e}")
+#         data = {"error": True, "message": "伺服器內部錯誤"}
+#         return JSONResponse(status_code=500, content=data, media_type="application/json")
