d9d412283779f120ca1fe44c9651e402fa8c3351
劉晏儒 <ruru@liuyanrudeMacBook-Pro.local>
2024-06-17 00:51:10 +0800

    test

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ def create_db_pool(db):
     dbconfig = {
         "host": os.environ.get("MYSQL_HOST", "localhost"),
         "database": db,
+        "port":3306,
         "password": os.environ.get("MYSQL_PASSWORD"),
     }
     return mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool", pool_size=10, **dbconfig)
